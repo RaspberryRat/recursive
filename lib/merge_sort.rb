2cce7ae2acf8d9c427758a0ccb422f0ff024b6b9
Graham <git@grahamirvine.com>
2022-06-15 20:09:19 -0600

    GOT IT! problem was not returning num_array at base case so merge_sort was returning nil

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -1,32 +1,25 @@
 require "pry-byebug"
 arr = [4, 8, 6, 2, 1, 7, 5, 3]
-arr2 = [4,8,6,2]
+arr2 = [8,4,6,2]
 arr3 = [8, 4]
 
 def merge_sort(num_array, sorted_array = [])
-  return if num_array.length < 2
-
-
+  return num_array if num_array.length < 2
 
   left_side = num_array[0..num_array.length / 2 - 1]
   right_side = num_array[num_array.length / 2..]
 
-  binding.pry
-  merge_sort(left_side)
-  merge_sort(right_side)
-  merge(left_side, right_side, sorted_array)
-  print "\n#{sorted_array}\n"
-  
-  # if sorted_array > 1
-  # sorted.array.length
-  # if sorted_array == 1, return sorte
+  x = merge_sort(left_side)
+  y = merge_sort(right_side)
+  # problem is merge not using merge_sort array but why are they nil
+  merge(x, y, sorted_array)
 
+  print "\n#{sorted_array}\n"
   sorted_array
 end
 
-
 def merge(left_side, right_side, sorted_array)
-  binding.pry
+
   until left_side.empty? && right_side.empty? do
     if left_side.empty? && right_side.empty? == false
       sorted_array.push(right_side.shift)
@@ -44,9 +37,8 @@ def merge(left_side, right_side, sorted_array)
 end
 
 
-
-# print "Arr3: #{arr3.join(", ")}\n"
-# merge_sort(arr3)
-
 print "Arr2: #{arr2.join(", ")}\n"
 merge_sort(arr2)
+
+print "Arr: #{arr.join(", ")}\n"
+merge_sort(arr)
