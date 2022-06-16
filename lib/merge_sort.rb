64226a14d7ba4e14c2b635bb198f2125ff2f6b40
Graham <git@grahamirvine.com>
2022-06-16 12:03:18 -0600

    Refactored merge code to be easier to read.

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -1,31 +1,21 @@
-def merge_sort(num_array, sorted_array = [])
+def merge_sort(num_array)
   return num_array if num_array.length < 2
 
   left_side = merge_sort(num_array[0..num_array.length / 2 - 1])
   right_side = merge_sort(num_array[num_array.length / 2..])
-  merge(left_side, right_side, sorted_array)
-
-  sorted_array
+  merge(left_side, right_side)
 end
 
 # merges two arrays that are already in sorted order
-def merge(left_side, right_side, sorted_array)
-  until left_side.empty? && right_side.empty? do
-    if left_side.empty? && right_side.empty? == false
-      sorted_array.push(right_side.shift)
-    elsif left_side.empty? == false && right_side.empty?
-      sorted_array.push(left_side.shift)
-    elsif left_side[0] < right_side[0]
-      sorted_array.push(left_side.shift)
-    elsif left_side[0] > right_side[0]
-      sorted_array.push(right_side.shift)
-    elsif left_side[0] == right_side[0]
-      sorted_array.push(left_side.shift, right_side.shift)
-    else
-      puts "ERROR ERROR"
+def merge(left_side, right_side, sorted_array = [])
+  until left_side.empty?|| right_side.empty? do
+    if left_side[0] < right_side[0]
+      sorted_array << left_side.shift
+    elsif
+      sorted_array << right_side.shift
     end
   end
-  sorted_array
+  sorted_array + left_side + right_side
 end
 
 # creates a random array to sort
